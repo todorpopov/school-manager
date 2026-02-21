4c6f2bef7566ddf4b642d8e998bfb5214b02cdd4
todorpopov <todorpopov02@gmail.com>
2026-02-21 14:58:12 +0200

    Edit users migration

--- persistence/migrations/20260221125617_schema_migration_create_users_table.sql
+++ persistence/migrations/20260221125617_schema_migration_create_users_table.sql
@@ -6,7 +6,6 @@ CREATE TABLE IF NOT EXISTS users (
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    role VARCHAR(255) NOT NULL DEFAULT 'user',
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 -- +goose StatementEnd
